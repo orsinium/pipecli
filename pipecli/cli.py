e94dfff018ed3c0186dc7fe4e684f222dc6bcac7
Gram <master_fess@mail.ru>
2018-05-22 15:29:48 +0500

    +logging

--- pipecli/cli.py
+++ pipecli/cli.py
@@ -8,36 +8,28 @@ from prompt_toolkit.contrib.completers import WordCompleter
 # from prompt_toolkit import print_formatted_text, HTML
 
 from .core import Tree, commands
-
-
-class Formatter:
-    @staticmethod
-    def success(text):
-        return text
-        # return HTML('<ansigreen>{}</ansigreen>'.format(text))
-
-    @staticmethod
-    def error(text):
-        return text
-        # return HTML('<ansired>{}</ansired>'.format(text))
+from .logger import logger
 
 
 class ProxyTree:
-    def __init__(self):
-        self._tree = Tree()
+    def __init__(self, logger):
+        self._tree = Tree(logger)
 
     def getter(self, name, *args):
         method = getattr(self._tree, name)
         if not args:
-            return method(*args)
+            return method()
 
+        # replace command name to command object
         argspec = getargspec(method).args
         if len(argspec) >= 2 and argspec[1] == 'command':
             command = self._tree._get_command_by_name(args[0])
             if command is None:
-                return 'command not found'
-            return method(command, *args[1:])
+                self._tree.logger.error('command not found')
+                return
+            args[0] = command
 
+        # call method
         result = method(*args)
         if type(result) is list:
             result = '\n'.join(result)
@@ -47,6 +39,9 @@ class ProxyTree:
         return partial(self.getter, name)
 
     def push(self, command_name):
+        if command_name not in commands:
+            self._tree.logger.error('command not found')
+            return
         command = commands[command_name]
         return self._tree.push(command)
 
@@ -66,7 +61,7 @@ class ProxyTree:
 ACTIONS = tuple(action for action in dir(Tree) if not action.startswith('_'))
 COMMANDS = tuple(commands.keys())
 
-tree = ProxyTree()
+tree = ProxyTree(logger=logger)
 history = InMemoryHistory()
 suggest = AutoSuggestFromHistory()
 completer = WordCompleter(ACTIONS + COMMANDS)
@@ -86,8 +81,10 @@ def cli():
         try:
             result = getattr(tree, action)(*args)
         except Exception as e:
-            print(Formatter.error(e))
-        else:
+            logger.critical(e)
+            continue
+
+        if result is not None and type(result) is not bool:
             print(result)
 
 
--- pipecli/core/tree.py
+++ pipecli/core/tree.py
@@ -11,24 +11,30 @@ TreeElement = namedtuple('TreeElement', ['deepth', 'command', 'is_pointer'])
 
 
 class Tree:
-    def __init__(self):
+    def __init__(self, logger):
         self.root = Root()
         self.pointer = self.root
+        self.logger = logger
 
     def _get_parent(self, command=None, parent=None):
         if command is None:
             command = self.pointer
         if parent is None:
             parent = self.root
-        if command == parent:
+        if command in parent.subcommands:
             return parent
         for subcommand in parent.subcommands:
-            return self._get_parent(command, subcommand)
+            result = self._get_parent(command, subcommand)
+            if result:
+                return result
+        if parent == self.root:
+            self.logger.error('parent for command not found')
 
     def _get_command_by_name(self, name):
         for el in self.tree():
             if el.command.name == name:
                 return el.command
+        self.logger.error('command not found')
 
     def tree(self, command=None):
         if command is None:
@@ -44,39 +50,61 @@ class Tree:
         command = command()
         self.pointer.subcommands.append(command)
         self.pointer = command
+        self.logger.info('pushed')
+        return True
 
     def pop(self, command=None):
         if command is None:
             command = self.pointer
         self.pointer = self._get_parent(command)
+        if self.pointer is None:
+            return False
         self.pointer.subcommands = [cmd for cmd in self.pointer.subcommands if cmd is not command]
+        self.logger.info('poped')
+        return True
 
     def goto(self, command):
-        self.pointer = command
+        for el in self.tree():
+            if el.command is command:
+                self.pointer = command
+                return True
+        self.logger.error('command not found in tree')
+        return False
 
     def reset(self, *args):
         self.root = Root()
         self.pointer = self.root
+        self.logger.info('reseted')
+        return True
 
     def rename(self, command=None, name=None):
         if type(command) is str and name is None:
             command, name = name, command
+        if name is None:
+            self.logger.error('new name required')
+            return False
         if command is None:
             command = self.pointer
         command.name = name
+        self.logger.info('renamed')
+        return True
 
     def args(self, *args):
         command = self.pointer
-        command.update_args(args)
+        return command.update_args(args)
 
     def sources(self, *sources):
         command = self.pointer
         command.sources = set(sources)
+        self.logger.info('sources changed')
+        return True
 
     def flush(self, command=None):
         if command is None:
             command = self.pointer
         command.flush()
+        self.logger.info('flushed')
+        return True
 
     def load(self, path):
         exists = set(commands)
@@ -84,6 +112,9 @@ class Tree:
         if path.endswith('.py'):
             # get module info
             path = Path(path)
+            if not path.exists():
+                self.logger.error('file not found')
+                return []
             module_name = path.stem
             full_path = path.absolute()
             # load module
@@ -92,15 +123,25 @@ class Tree:
             spec.loader.exec_module(module)
         # module name
         else:
-            import_module(path)
+            try:
+                import_module(path)
+            except ImportError:
+                self.logger.error('module not found')
+                return []
         # return list of loaded commands
         loaded = set(commands) - exists
+        if loaded:
+            self.logger.info('loaded')
+        else:
+            self.logger.warning('loaded 0 commands')
         return sorted(list(loaded))
 
     def list(self, pattern=None):
         result = list(commands)
         if pattern:
             result = fnmatch.filter(names=result, pattern=pattern)
+        if not result:
+            self.logger.warning('no matches found')
         return sorted(result)
 
     def run(self):
